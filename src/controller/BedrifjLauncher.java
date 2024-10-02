818057ee337956f323fa920eef634e3120b637e4
furkan <frknlty4@gmail.com>
2024-10-02 15:18:11 +0200

    Version 2

--- src/controller/BedrifjLauncher.java
+++ src/controller/BedrifjLauncher.java
@@ -2,6 +2,8 @@ package controller;
 
 import model.Persoon;
 
+import java.util.Arrays;
+
 /**
  * @author Furkan Altay
  * Purpose for the class
@@ -17,16 +19,31 @@ public class BedrifjLauncher {
         Persoon baas = new Persoon("Mark", "Den Haag", 10000);
         System.out.println("Aantal personen: " + Persoon.aantalPersonen);
         //Tijdens het instantieren van baas wordt aantalPersonen verhoogd met 1(en is dus gelijk aan 1)
-        System.out.println("Nummer van medewerker is " + baas.personeelsNummer);
+        System.out.println("Nummer van medewerker is " + baas.getPersoneelsNummer());
 
         Persoon medewerker = new Persoon("Caroline", "Delft", 4000);
         System.out.println("Aantal personen: " + Persoon.aantalPersonen);
-        System.out.println("Nummer van medewerker is " + medewerker.personeelsNummer);
+        System.out.println("Nummer van medewerker is " + medewerker.getPersoneelsNummer());
         // 15
         Persoon assistent = new Persoon("Klaas");
         Persoon manager = new Persoon();
         System.out.println(Persoon.aantalPersonen);
-        System.out.printf("%s verdient %.2f per jaar\n",baas.naam, baas.berekenJaarInkomen());
-        System.out.printf("%s woont is %s\n", assistent.naam, assistent.woonplaats);
+
+        Persoon[] medewerkers = new Persoon[3];
+        medewerkers[0] = new Persoon("Mark","Leek",10000);
+        medewerkers[1] = new Persoon("Caroline","Marum",4000);
+        medewerkers[2] = new Persoon("Arjen","Roden",-200);
+
+        for (int teller = 0; teller < medewerkers.length; teller++) {
+            System.out.printf("%s verdient %.2f ",medewerkers[teller].getNaam(), medewerkers[teller].getMaandsalaris());
+            if (medewerkers[teller].heeftRechtOpBonus()) {
+                System.out.println("en wel recht op een bonus.");
+            }
+            else {
+                System.out.println("en heeft geen recht op bonus.");
+            }
+        }
+
+
     }
 }
--- src/model/Persoon.java
+++ src/model/Persoon.java
@@ -5,40 +5,79 @@ package model;
  * Purpose for the class
  */
 public class Persoon {
+    private static double GRENSWAARDE_BONUS = 4500.0;
     public static final String DEFAULT_NAAM = "onbekend";
     public static final String DEFAULT_WOONPLAATS = "onbekend";
     public static final int DEFAULT_MAANDSALARIS = 0;
     public static final int MAANDEN_PER_JAAR = 12;
     public static int aantalPersonen = 0;
 
-    public String naam;
-    public String woonplaats;
-    public double maandsalaris;
-    public int personeelsNummer;
+    private String naam;
+    private String woonplaats;
+    private double maandsalaris;
+    private int personeelsNummer;
 
     public Persoon(String naam, String woonplaats, double maandsalaris) {
         this.naam = naam;
         this.woonplaats = woonplaats;
-        this.maandsalaris = maandsalaris;
+        setMaandsalaris(maandsalaris);
         this.personeelsNummer = ++aantalPersonen;
     }
 
     public Persoon(String naam) {
-        this.naam = naam;
-        this.woonplaats = DEFAULT_WOONPLAATS;
-        this.maandsalaris = DEFAULT_MAANDSALARIS;
-        this.personeelsNummer = ++aantalPersonen;
+        this(naam,DEFAULT_WOONPLAATS,DEFAULT_MAANDSALARIS);
     }
 
     public Persoon(){
-        this.naam = DEFAULT_NAAM;
-        this.woonplaats = DEFAULT_WOONPLAATS;
-        this.maandsalaris = DEFAULT_MAANDSALARIS;
-        this.personeelsNummer = ++aantalPersonen;
+        this(DEFAULT_NAAM);
+    }
+
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public String getWoonplaats() {
+        return woonplaats;
+    }
+
+    public void setWoonplaats(String woonplaats) {
+        this.woonplaats = woonplaats;
+    }
+
+    public double getMaandsalaris() {
+        return maandsalaris;
+    }
+
+    public void setMaandsalaris(double maandsalaris) {
+       if (maandsalaris < 0) {
+           System.out.println("Het bedrag kan niet negatief zijn.");
+           this.maandsalaris = DEFAULT_MAANDSALARIS ;
+       }
+       else {
+           this.maandsalaris = maandsalaris;
+       }
+    }
+
+    public int getPersoneelsNummer() {
+        return personeelsNummer;
+    }
+
+    public void setPersoneelsNummer(int personeelsNummer) {
+        this.personeelsNummer = personeelsNummer;
     }
 
     public double berekenJaarInkomen(){
         return MAANDEN_PER_JAAR * maandsalaris;
     }
 
+    public boolean heeftRechtOpBonus() {
+        return maandsalaris >= GRENSWAARDE_BONUS;
+    }
+
 }
+
